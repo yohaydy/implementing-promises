4645f84810f4c1f2b6723acb8accc4f596dd8c9a
Oskars Rukmans <oskars.rukmans@gmail.com>
2017-11-25 19:22:26 +0200

    The most basic implementation

--- src/implementingPromises.js
+++ src/implementingPromises.js
@@ -1,7 +1,26 @@
-function MyPromise(){
-    
-}
+function MyPromise(executorfn) {
+	var value;
+	var state = "pending";
+	var deferred;
 
-// var MyPromise2 = Promise
+	function resolver(_value) {
+		value = _value;
+		if (deferred) {
+			deferred(value);
+		}
+	}
+
+	function then(resolutionHandler) {
+		if (state === "fulfilled") {
+			// resolver() has been called already
+			resolutionHandler(value);
+		} else if (state === "pending") {
+			// resolver() has not been called yet
+			deferred = resolutionHandler;
+		}
+	}
+
+	executorfn(resolver);
+}
 
 module.exports = MyPromise;
